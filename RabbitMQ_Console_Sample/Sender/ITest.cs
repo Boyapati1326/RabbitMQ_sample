69def9aab85d1ee6a7777b3a48e65cf5a5625f14
Boyapati1326 <boyapati1326@gmail.com>
2024-05-27 23:24:26 -0400

    Intial Commit

---
+++ RabbitMQ_Console_Sample/Sender/ITest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Send
+{
+    internal interface ITest
+    {
+        void M() { Console.WriteLine("IA.M"); }
+
+    }
+}
---
+++ RabbitMQ_Console_Sample/Sender/Send.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using Send;
+
+namespace Send
+{
+    public class Send : ITest
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hi I am a RabbitMQ Sender");
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            for (int i = 0; i <= 5; i++)
+            {
+                channel.QueueDeclare(queue: "hello",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+                string message = Console.ReadLine();
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: string.Empty,
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine($" [x] Sent {message}");
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+            Console.WriteLine(Testing());
+            Console.ReadKey();
+
+
+
+        }
+
+        public static string Testing()
+        {
+            string val = string.Empty;
+            int[] a = new int[3];
+            try
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    a[i] = i;
+                }
+                return "I am done";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("I am exception");
+                return "I am exception";
+            }
+            finally
+            {
+                Console.WriteLine("i am finally");
+                val = "i am finally";
+            }
+            return val;
+        }
+    }
+}
